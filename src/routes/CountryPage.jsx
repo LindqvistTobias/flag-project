84431da6e9b279d3703ca320b95128d301672a36
Tobias <lokwavetechnologies@gmail.com>
2024-05-05 13:40:07 +0200

    Functionallity and almost all css done. Still need to do dropdown and searchbar. Also need to do more repsonsivity.

--- src/routes/CountryPage.jsx
+++ src/routes/CountryPage.jsx
@@ -1,28 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
-import './CountryPage.css';
-import arrowLeftWhite from '../assets/arrow-left.svg';
-import arrowLeftDark from '../assets/arrow-left-dark.svg';
-import { useDarkMode } from '../../DarkModeContext';
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import "./CountryPage.css";
+import arrowLeftWhite from "../assets/arrow-left.svg";
+import arrowLeftDark from "../assets/arrow-left-dark.svg";
+import { useDarkMode } from "../../DarkModeContext";
 
 const CountryPage = () => {
   const navigate = useNavigate();
   const { name } = useParams();
   const { isDarkMode } = useDarkMode();
-
   const [currentCountry, setCurrentCountry] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchCountryData = async () => {
+      setIsLoading(true);
       try {
-        const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
+        const response = await axios.get(
+          `https://restcountries.com/v3.1/name/${name}`
+        );
         const countryData = response.data[0]; // Assuming the first item is the matched country
         setCurrentCountry(countryData);
       } catch (error) {
-        console.error('Error fetching country data:', error);
+        console.error("Error fetching country data:", error);
         setCurrentCountry(null); // Handle error by setting currentCountry to null
       }
+      setIsLoading(false);
     };
 
     fetchCountryData();
@@ -33,14 +37,38 @@ const CountryPage = () => {
   };
 
   const handleGoBack = () => {
-    navigate('/');
+    navigate("/");
   };
 
-  const renderCountryInfo = () => {
-    if (!currentCountry) {
-      return <div>Country not found.</div>;
+  const handleBorderCountryClick = async (cca3Code) => {
+    try {
+      // Fetch country data based on the cca3Code
+      const response = await axios.get(
+        `https://restcountries.com/v3.1/alpha/${cca3Code}`
+      );
+  
+      // Check if response data is valid and contains the country name
+      if (
+        Array.isArray(response.data) &&
+        response.data.length > 0 &&
+        response.data[0].name &&
+        response.data[0].name.common
+      ) {
+        const matchedCountryName = response.data[0].name.common;
+  
+        // Navigate to the matched country's route
+        navigate(`/${matchedCountryName}`);
+      } else {
+        throw new Error("Invalid response data or missing country name");
+      }
+    } catch (error) {
+      console.error("Error fetching border country data:", error.message);
+      // Handle error if the border country data cannot be fetched
+      // Display an error message or take appropriate actions
     }
+  };
 
+  if (isLoading) {
     return (
       <div className="main-container">
         <div className="back-container" onClick={handleGoBack}>
@@ -48,55 +76,100 @@ const CountryPage = () => {
           <p className="back-button">BACK</p>
         </div>
         <div className="card-container">
-          <img id="flag-img" src={currentCountry.flags.png} alt={`Flag of ${currentCountry.name.common}`} />
+          <div
+            className="loading-flag"
+            style={{ animation: "blink 1s infinite" }}
+          ></div>
           <div className="text-container">
-            <h2>{currentCountry.name.common}</h2>
-            <div className="text-info-container">
-              <div className="text-info">
-                <p>Population: {currentCountry.population}</p>
-              </div>
-              <div className="text-info">
-                <p>Region: {currentCountry.region}</p>
-              </div>
-              <div className="text-info">
-                <p>Capital: {currentCountry.capital}</p>
-              </div>
-              <div className="text-info">
-                <p>Native name: {currentCountry.nativeName?.common || 'Unknown Native Name'}</p>
-              </div>
-              <div className="text-info">
-                <p>Top Level Domain: {currentCountry.tld}</p>
-              </div>
-              <div className="text-info">
-                <p>Currencies: {Object.values(currentCountry.currencies || {}).map(currency => currency.name).join(', ') || 'Unknown Currency'}</p>
-              </div>
-              <div className="text-info">
-                <p>Languages: {Object.values(currentCountry.languages || {}).map(language => language.name).join(', ') || 'Unknown Language'}</p>
-              </div>
-            </div>
-            <div className="border-countries">
-              <p>Border Countries:</p>
-              <ul>
-                {currentCountry.borders && currentCountry.borders.length > 0 ? (
-                  currentCountry.borders.map((border) => (
-                    <li key={border}>
-                      <button className="border" onClick={() => handleBorderCountryClick(border)}>
-                        {border || 'Unknown Country'}
-                      </button>
-                    </li>
-                  ))
-                ) : (
-                  <li>No neighbors</li>
-                )}
-              </ul>
-            </div>
+            <h2>Loading...</h2>
           </div>
         </div>
       </div>
     );
-  };
+  }
 
-  return renderCountryInfo();
+  const getFirstNativeNameCommon = () => {
+    const { nativeName } = currentCountry?.name || {};
+    const firstNativeName = Object.values(nativeName || {})[0];
+    return firstNativeName?.common || 'Unknown Native Name';
+};
+
+const getFirstLanguageName = () => {
+    const languageEntries = Object.entries(currentCountry?.languages || {});
+    return languageEntries.length > 0 ? languageEntries[0][1] || 'Unknown Language' : 'Unknown Language';
+};
+
+  return (
+    <div className="main-container">
+      <div className="back-container" onClick={handleGoBack}>
+        <img src={getArrowIconSource()} alt="arrow" id="arrow" />
+        <p className="back-button">BACK</p>
+      </div>
+      <div className="card-container">
+        <img
+          id="flag-img"
+          src={currentCountry.flags.png}
+          alt={`Flag of ${currentCountry.name.common}`}
+        />
+        <div className="text-container">
+          <h2>{currentCountry.name.common}</h2>
+          <div className="text-info-container">
+            <div className="text-info-small-container">
+              <div className="text-info">
+                <p>Population:</p> {currentCountry.population}
+              </div>
+              <div className="text-info">
+                <p>Region:</p> {currentCountry.region}
+              </div>
+              <div className="text-info">
+                <p>Capital:</p> {currentCountry.capital}
+              </div>
+              <div className="text-info">
+                <p>Native name:</p>{" "}
+                {getFirstNativeNameCommon()}
+              </div>
+            </div>
+            <div className="text-info-small-container">  
+              <div>
+                <div className="text-info">
+                  <p>Top Level Domain:</p> {currentCountry.tld}
+                </div>
+                <div className="text-info">
+                  <p>Currencies:</p>{" "}
+                  {Object.values(currentCountry.currencies || {})
+                    .map((currency) => currency.name)
+                    .join(", ") || "Unknown Currency"}
+                </div>
+                <div className="text-info">
+                  <p>Languages:</p>{" "}
+                  {getFirstLanguageName()}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="border-countries">
+            <p>Border Countries:</p>
+            <ul>
+              {currentCountry.borders && currentCountry.borders.length > 0 ? (
+                currentCountry.borders.map((border) => (
+                  <li key={border}>
+                    <button
+                      className="border"
+                      onClick={() => handleBorderCountryClick(border)}
+                    >
+                      {border || "Unknown Country"}
+                    </button>
+                  </li>
+                ))
+              ) : (
+                <li>No neighbors</li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CountryPage;
